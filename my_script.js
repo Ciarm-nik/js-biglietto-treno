ab37a301b9a69f6d3f9e8374de0f804db0782ccb
Ciarm-nik <nikcia@libero.it>
2021-04-08 16:06:01 +0200

    Inserimento prezzo

--- my_script.js
+++ my_script.js
@@ -1,5 +1,13 @@
-var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"))
-var età = parseInt(prompt("Quanti anni hai?")) 
+var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+console.log("chilometri", chilometri);
 
-console.log("chilometri", chilometri)
-console.log("età", età)
+var età = parseInt(prompt("Quanti anni hai?"));
+console.log("età", età);
+
+var biglietto = (0.21);
+
+if
+
+var prezzo = chilometri * biglietto;
+console.log("prezzo", prezzo);
+
