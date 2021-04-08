1cdfcde3083be540e84acdb4cf5a242d62638814
Ciarm-nik <nikcia@libero.it>
2021-04-08 15:20:03 +0200

    Inizio esercizio

---
+++ my_script.js
@@ -0,0 +1,5 @@
+var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"))
+var età = parseInt(prompt("Quanti anni hai?")) 
+
+console.log("chilometri", chilometri)
+console.log("età", età)
