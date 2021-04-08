7adec1cb33b757ff25e1b181943bfa40e28458a9
Ciarm-nik <nikcia@libero.it>
2021-04-08 17:31:55 +0200

    Aggiunta tofixed per output

--- my_script.js
+++ my_script.js
@@ -1,28 +1,30 @@
 var chilometri = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 console.log("chilometri", chilometri);
 
-var età = parseInt(prompt("Quanti anni hai?"));
-console.log("età", età);
+var eta = parseInt(prompt("Quanti anni hai?"));
+console.log("eta", eta);
 
 var biglietto = (0.21);
 
-if (età < 18) {
+if (eta < 18) {
     //applico uno sconto del 20%
     var prezzo = chilometri * biglietto;
     var risultato = prezzo - prezzo*0.2;
     //Stampo il risultato
-    console.log(risultato);
-} else if (età > 18 && età <65) {
+    console.log("risultato", risultato.toFixed(2));
+
+} if (eta > 18 && eta < 65) {
     //nessuno sconto
     var prezzo = chilometri * biglietto;
     //Stampo il risultato
-    console.log(prezzo);
-} else if (età > 65) {
-      //applico uno sconto del 65%
+    console.log("prezzo", prezzo.toFixed(2));
+
+} if (eta > 65) {
+      //applico uno sconto del 40%
       var prezzo = chilometri * biglietto;
       var risultato = prezzo - prezzo*0.4;
       //Stampo il risultato 
-      console.log(risultato);
+      console.log("risultato", risultato.toFixed(2));
 }
   
 
