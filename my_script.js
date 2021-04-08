34175d614361022edc8023022e4a2757d3958b9a
Ciarm-nik <nikcia@libero.it>
2021-04-08 16:49:04 +0200

    Inserisco prime due condizioni

--- my_script.js
+++ my_script.js
@@ -6,8 +6,18 @@ console.log("età", età);
 
 var biglietto = (0.21);
 
-if
+if (età < 18) {
+    //applico uno sconto del 20%
+    var prezzo = chilometri * biglietto;
+    var risultato = prezzo - prezzo*0.2;
+    //Stampo il risultato
+    console.log(risultato);
+} else if (età > 18 && età <65) {
+    //nessuno sconto
+    var prezzo = chilometri * biglietto;
+    //Stampo il risultato
+    console.log(prezzo);
+}
 
-var prezzo = chilometri * biglietto;
-console.log("prezzo", prezzo);
+
 
