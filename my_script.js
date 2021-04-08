03cadc18f4aa73b84deaf9cb6fe1b0af45fa5a08
Ciarm-nik <nikcia@libero.it>
2021-04-08 18:13:20 +0200

    Aggiunta if/controllo e corretto indentazione

--- my_script.js
+++ my_script.js
@@ -6,29 +6,32 @@ console.log("eta", eta);
 
 var biglietto = (0.21);
 
-if (eta < 18) {
-    //applico uno sconto del 20%
-    var prezzo = chilometri * biglietto;
-    var risultato = prezzo - prezzo*0.2;
-    //Stampo il risultato
-    console.log("risultato", risultato.toFixed(2));
-    document.getElementById("prezzo_biglietto").innerHTML = risultato.toFixed(2)
-
-} else if (eta >= 18 && eta < 65) {
-    //nessuno sconto
-    var prezzo = chilometri * biglietto;
-    //Stampo il risultato
-    console.log("prezzo", prezzo.toFixed(2));
-    document.getElementById("prezzo_biglietto").innerHTML = prezzo.toFixed(2)
-
-} else if (eta > 65) {
-      //applico uno sconto del 40%
-      var prezzo = chilometri * biglietto;
-      var risultato = prezzo - prezzo*0.4;
-      //Stampo il risultato 
-      console.log("risultato", risultato.toFixed(2));
-      document.getElementById("prezzo_biglietto").innerHTML = risultato.toFixed(2)
-}
+if (Number.isNaN(chilometri) || Number.isNaN(eta)) {
+    alert("Dati mancanti")
+} else {
+    if (eta < 18) {
+        //applico uno sconto del 20%
+        var prezzo = chilometri * biglietto;
+        var risultato = prezzo - prezzo*0.2;
+        //Stampo il risultato
+        console.log("risultato", risultato.toFixed(2));
+        document.getElementById("prezzo_biglietto").innerHTML = risultato.toFixed(2)
+    } else if (eta >= 18 && eta < 65) {
+        //nessuno sconto
+        var prezzo = chilometri * biglietto;
+        //Stampo il risultato
+        console.log("prezzo", prezzo.toFixed(2));
+        document.getElementById("prezzo_biglietto").innerHTML = prezzo.toFixed(2)
+    
+    } else if (eta > 65) {
+          //applico uno sconto del 40%
+          var prezzo = chilometri * biglietto;
+          var risultato = prezzo - prezzo*0.4;
+          //Stampo il risultato 
+          console.log("risultato", risultato.toFixed(2));
+          document.getElementById("prezzo_biglietto").innerHTML = risultato.toFixed(2)
+    }
+} 
 
 
 
